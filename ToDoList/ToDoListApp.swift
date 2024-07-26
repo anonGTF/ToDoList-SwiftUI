1ec25571628ce28eba9aaf613958abfc9cc1ef40
galih.tegar <galih.tegar.febrianto@bni.co.id>
2024-07-26 10:47:33 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Galih Tegar Febrianto on 26/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
